c6f9a51046bafff6e74c2b89f56197ea9ebb4d0a
yuanj <1107678466@qq.com>
2017-06-27 23:07:00 +0800

    【add】"年级默认金额"

---
+++ torch_pj/src/main/java/com/torch/domain/model/gradeMoney/DictGradeMoneyRepository.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017
+ * 成都博智云创科技有限公司.
+ */
+package com.torch.domain.model.gradeMoney;
+
+import com.torch.domain.model.audit.Audit;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author yuanj 2017-01-16 11:44:31
+ * @version 1.0
+ */
+public interface DictGradeMoneyRepository extends CrudRepository<DictGradeMoney, Long> {
+
+}
